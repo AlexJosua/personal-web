c4116405daa600f9f309d950ca5200baca8eaa8f
AlexJosua <130466775+AlexJosua@users.noreply.github.com>
2025-02-23 19:27:33 +0700

    update lauouting navbar membuat helper untuk membanding kan variabel dan keperluan waktu menjalankan blog list dan create blog menjalankan fungsi submit blog menggunakan method post

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+const { equal } = require("assert");
 const express = require("express");
 const app = express();
 const port = 3000;
+const hbs = require("hbs");
+const path = require("path");
+
+const { formatDateToWIB, getRelativeTime } = require("./utils/time");
 
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./views"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("assets"));
 
-//  MENGACU KE HALAMAN HOME
+hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+hbs.registerHelper("equal", function (a, b) {
+  return a === b;
+});
+
+hbs.registerHelper("formatDateToWIB", formatDateToWIB);
+hbs.registerHelper("getRelativeTime", getRelativeTime);
+
+let blogs = [];
+
+// HALAMAN HOME
 app.get("/", (req, res) => {
   // res.send("Hello Express! Ini Halaman Saya yang pertama");
   res.render("index");
@@ -18,7 +36,39 @@ app.get("/index", (req, res) => {
 });
 
 app.get("/blog", (req, res) => {
-  res.render("blog");
+  console.log(blogs);
+  res.render("blog-list", { blogs: blogs });
+});
+
+app.get("/blog-create", (req, res) => {
+  res.render("blog-create");
+});
+
+//SUBMIT NEW BLOG
+app.post("/blog-create", (req, res) => {
+  // const title = req.body.title;
+  // const content = req.body.content;
+
+  const { title, content } = req.body; // ttile dan content adalah properti milik req.body
+  console.log("Judulnya title", title);
+  console.log("Content nya ", content);
+
+  let image = "https://picsum.photos/200/120";
+  let blog = {
+    title: title,
+    content: content,
+    image: image,
+    author: "Leo G",
+    postedAt: new Date(),
+  };
+
+  blogs.push(blog);
+
+  res.redirect("/blog");
+});
+
+app.get("/blog/:id", (req, res) => {
+  res.render("blog-detail");
 });
 
 app.get("/testimonial", (req, res) => {
@@ -47,5 +97,5 @@ app.get("/blog", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`My personal web app listening on port ${port}`);
+  console.log(`My personal web app listening on port  ${port}`);
 });
---
+++ utils/time.js
@@ -0,0 +1,74 @@
+function formatDateToWIB(date) {
+  //   let date = new Date();
+  let monthsList = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Mei",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Des",
+  ];
+
+  let day = date.getDate().toString().padStart(2, "0");
+  let month = monthsList[date.getMonth()];
+  let year = date.getFullYear();
+  //   console.log(year);
+
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+
+  //   console.log(`sekarang jam ${hours}:${minutes}`);
+
+  let formattedDate = `${day} ${month} ${year} ${hours}:${minutes} WIB`;
+
+  // console.log("Tanggal Format Baru", formattedDate);
+
+  return formattedDate;
+}
+
+function getRelativeTime(postTime) {
+  let now = new Date();
+  console.log("Waktu Sekarang :", now);
+  console.log("Waktu User Post :", postTime);
+
+  let diffTime = now - postTime;
+  console.log("diffTime waktu nya : ", diffTime);
+
+  let diffInSeconds = Math.floor((now - postTime) / 1000);
+  console.log("Ini hasil selisih : ", diffInSeconds);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} Seconds ago`;
+  }
+  let diffInMinutes = Math.floor(diffInSeconds / 60);
+
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes} minutes ago`;
+  }
+
+  let diffInHours = Math.floor(diffInMinutes / 60);
+
+  if (diffInHours < 24) {
+    return `${diffInHours} hours ago`;
+  }
+
+  let diffInDays = Math.floor(diffInHours / 24);
+
+  if (diffInDays < 30) {
+    return `${diffInDays} days ago`;
+  }
+
+  let diffInMonth = Math.floor(diffInDays / 30);
+  return `${diffInMonth} month${diffInMonth === 1 ? "" : "s"} ago`;
+}
+
+module.exports = {
+  formatDateToWIB,
+  getRelativeTime,
+};
